075aebf43195b36e5a846ec31307353515e8cf3f
alesya <kropka171001@gmail.com>
2020-09-04 13:57:53 +0300

    search professors by subject

--- search.sql
+++ search.sql
@@ -2,3 +2,5 @@ USE professorsdb;
 select * from professor;
 USE student_groupdb;
 select * from student_group where speciality= 'ЭВМ';
+USE studying_proccessdb;
+select distinct professor_id, number_of_class from studying_proccess where subject_id = '18П';
