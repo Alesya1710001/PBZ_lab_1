9cecd04218533e742e5ae738ba0850ad4904ff8c
alesya <kropka171001@gmail.com>
2020-09-06 10:12:37 +0300

    1-9 question

--- search.sql
+++ search.sql
@@ -1,6 +1,62 @@
-USE professorsdb;
+USE studying_information;
+-- Получить полную информацию обо всех преподавателях.
 select * from professor;
-USE student_groupdb;
+
+-- Получить полную информацию обо всех студенческих группах на специальности ЭВМ.
 select * from student_group where speciality= 'ЭВМ';
-USE studying_proccessdb;
+
+-- Получить личный номер преподавателя и номера аудиторий, в которых они преподают предмет с кодовым номером 18П.
 select distinct professor_id, number_of_class from studying_proccess where subject_id = '18П';
+
+-- Получить  номера предметов и названия предметов, которые ведет преподаватель Костин.
+select distinct subject_number, title from subject 
+inner join studying_proccess
+on studying_proccess.subject_id = subject.subject_number
+inner join professor
+on professor.professors_number = studying_proccess.professors_id
+where professor.surname = 'Костин';
+
+-- Получить номер группы, в которой ведутся предметы преподавателем Фроловым.
+select distinct group_id from studying_proccess
+inner join professor
+on professor.professors_number = studying_proccess.professors_id
+where professor.surname = 'Фролов';
+
+-- Получить информацию о предметах, которые ведутся на специальности АСОИ.
+select distinct * from subject where speciality = 'АСОИ';
+
+-- Получить информацию о преподавателях, которые ведут предметы на специальности АСОИ.
+select distinct * from professor where speciality like '%АСОИ%';
+
+-- Получить фамилии преподавателей, которые ведут предметы в 210 аудитории.
+select distinct surname from professor 
+inner join studying_proccess
+on studying_proccess.professors_id = professor.professors_number
+where studying_proccess.number_of_class= 210;
+
+-- Получить названия предметов и названия групп, которые ведут занятия в аудиториях с 100 по 200. 
+select distinct subject.title as 'subject_name', student_group.title as 'group_name' from studying_proccess
+inner join subject
+on  subject.subject_number = studying_proccess.subject_id
+inner join  student_group
+on student_group.group_number = studying_proccess.group_id
+where number_of_class between 100 and 200;
+
+-- Получить пары номеров групп с одной специальности.
+
+-- Получить общее количество студентов, обучающихся на специальности ЭВМ.
+-- Получить номера преподавателей, обучающих студентов по специальности ЭВМ.
+-- Получить номера предметов, изучаемых всеми студенческими группами.
+-- Получить фамилии преподавателей, преподающих те же предметы, что и преподаватель преподающий предмет с номером 14П.
+-- Получить информацию о предметах, которые не ведет преподаватель с личным номером 221П.
+-- Получить информацию о предметах, которые не изучаются в группе М-6.
+-- Получить информацию о доцентах, преподающих в группах 3Г и 8Г.
+-- Получить номера предметов, номера преподавателей, номера групп, в которых ведут занятия преподаватели с кафедры ЭВМ, имеющих специальность АСОИ.
+-- Получить номера групп с такой же специальностью, что и специальность преподавателей.
+-- Получить номера преподавателей с кафедры ЭВМ, преподающих предметы по специальности, совпадающей со специальностью студенческой группы. 
+-- Получить специальности студенческой группы, на которых работают преподаватели кафедры АСУ.
+-- Получить номера предметов, изучаемых группой АС-8.
+-- Получить номера студенческих групп, которые изучают те же предметы, что и студенческая группа АС-8.
+-- Получить номера студенческих групп, которые не изучают предметы, преподаваемых в студенческой группе АС-8.
+-- Получить номера студенческих групп, которые не изучают предметы, преподаваемых преподавателем 430Л.
+-- Получить номера преподавателей, работающих с группой Э-15, но не преподающих предмет 12П.
--- studying_informationdb.sql
+++ studying_informationdb.sql
@@ -1,14 +1,14 @@
 CREATE DATABASE studying_information;
 USE studying_information;
 CREATE TABLE professor(
-	id char(4),
+	professors_number char(4),
     surname TEXT,
     post TEXT,
     department TEXT,
     speciality char(30),
     phone_number INTEGER
 );
-INSERT INTO professor (id, surname, post, department, speciality, phone_number) 
+INSERT INTO professor (professors_number, surname, post, department, speciality, phone_number) 
 VALUES ('221Л', 'Фролов', 'Доцент', 'ЭВМ', 'АСОИ ЭВМ', 487 ), 
 ('222Л', 'Костин', 'Доцент', 'ЭВМ', 'ЭВМ', 543 ),
 ('225Л', 'Бойко', 'Профессор', 'АСУ', 'АСОИ ЭВМ', 112 ),
@@ -16,38 +16,38 @@ VALUES ('221Л', 'Фролов', 'Доцент', 'ЭВМ', 'АСОИ ЭВМ', 48
 ('110Л', 'Петров', 'Ассистент', 'Экономики', 'Международная экономика', 324 );
 
 CREATE TABLE student_group(
-	id char(3),
+	group_number char(3),
     title char(5),
     number_of_students INTEGER, 
 	speciality char(30),
     surname_of_headman TEXT
 );
-INSERT INTO student_group (id, title, number_of_students, speciality, surname_of_headman) 
+INSERT INTO student_group (group_number, title, number_of_students, speciality, surname_of_headman) 
 VALUES ('8Г', 'Э-12', 18, 'ЭВМ', 'Иванова' ),('7Г', 'Э-15', 22, 'ЭВМ', 'Сеткин' ),
 ('4Г', 'АС-9', 24, 'АСОИ', 'Балабанов' ),('3Г', 'АС-8', 20, 'АСОИ', 'Чижов' ),
 ('17Г', 'С-14', 29, 'СД', 'Амросов' ),('12Г', 'М-6', 16, 'Международная экономика', 'Трубин' ),
 ('10Г', 'Б-4', 21, 'Бухучет', 'Зязютник' );
 
 CREATE TABLE subject(
-	id char(3),
+	subject_number char(3),
     title TEXT,
     number_of_hours INTEGER, 
 	speciality char(30),
     semester INTEGER
 );
-INSERT INTO subject (id, title, number_of_hours, speciality, semester) 
-VALUES ('12П', 'Мини ЭВМ', 36, 'ЭВМ', 1 ),('14П', 'ПЭВМ', 72, 'ЭВМ', 2 ), ('17П', 'СУБД ПК', 48, 'ФСОИ', 4 ),
+INSERT INTO subject (subject_number, title, number_of_hours, speciality, semester) 
+VALUES ('12П', 'Мини ЭВМ', 36, 'ЭВМ', 1 ),('14П', 'ПЭВМ', 72, 'ЭВМ', 2 ), ('17П', 'СУБД ПК', 48, 'АСОИ', 4 ),
 ('18П', 'ВКСС', 52, 'АСОИ', 6 ),('34П', 'Физика', 30, 'СД', 6 ),('22П', 'Аудит', 24, 'Бухучета', 3 );
 
 CREATE TABLE studying_proccess(
-	gruop_id char(3) references student_group(id) on delete cascade,
-    subject_id char(3) references subject(id) on delete cascade,
-    professor_id char(4) references professor(id) on delete cascade,
+	group_id char(3) references student_group(group_number) on delete cascade,
+    subject_id char(3) references subject(subject_number) on delete cascade,
+    professors_id char(4) references professor(professors_number) on delete cascade,
     number_of_class INTEGER,
-    primary key (gruop_id, subject_id, professor_id, number_of_class)
+    primary key (group_id, subject_id, professors_id, number_of_class)
 );
 
-INSERT INTO studying_proccess (gruop_id, subject_id, professor_id, number_of_class) 
+INSERT INTO studying_proccess (group_id, subject_id, professors_id, number_of_class) 
 VALUES ('8Г', '12П', '222Л', 112 ), ('8Г', '14П', '221Л', 220 ),('8Г', '17П', '222Л', 112 ),
 ('7Г', '14П', '221Л', 220 ),('7Г', '17П', '222Л', 241 ),('7Г', '18П', '225Л', 210 ),
 ('4Г', '12П', '222Л', 112 ),('4Г', '18П', '225Л', 210 ),('3Г', '12П', '222Л', 112 ),
