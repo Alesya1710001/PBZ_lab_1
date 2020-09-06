72b0b5ee327089e1e1944e989a76072e629ca69a
alesya <kropka171001@gmail.com>
2020-09-06 22:49:54 +0300

    some else question (>10)

--- search.sql
+++ search.sql
@@ -67,15 +67,69 @@ having count(group_id) = (select count(*) from student_group ) ;
 
 
 
+
+
+
+
+
+
+
+
 -- Получить информацию о предметах, которые не ведет преподаватель с личным номером 221П.
+select distinct subject_number, title, number_of_hours, speciality, semester from subject
+where subject.subject_number not in (
+		select distinct studying_proccess.subject_id from studying_proccess
+		where studying_proccess.professors_id = '221Л'
+    );
+
 -- Получить информацию о предметах, которые не изучаются в группе М-6.
+select distinct subject.* from subject
+where subject.subject_number not in (
+	select distinct studying_proccess.subject_id from studying_proccess
+		inner join student_group
+			on student_group.group_number = studying_proccess.group_id
+	where student_group.title  = 'М-6'
+    );
+    
 -- Получить информацию о доцентах, преподающих в группах 3Г и 8Г.
+select distinct professor.* from professor
+	inner join studying_proccess
+		on studying_proccess.professors_id = professor.professors_number
+where professor.post ='Доцент' and studying_proccess.group_id in ('8Г','3Г');
+
 -- Получить номера предметов, номера преподавателей, номера групп, в которых ведут занятия преподаватели с кафедры ЭВМ, имеющих специальность АСОИ.
+select distinct subject_id, professors_id, group_id from studying_proccess
+	inner join professor
+		on professor.professors_number = studying_proccess.professors_id
+where professor.department = 'ЭВМ' and professor.speciality like '%АСОИ%';
+
 -- Получить номера групп с такой же специальностью, что и специальность преподавателей.
+select distinct group_number from student_group
+	inner join professor
+		on professor.speciality = student_group.speciality;
+
 -- Получить номера преподавателей с кафедры ЭВМ, преподающих предметы по специальности, совпадающей со специальностью студенческой группы. 
+select distinct professors_number from professor
+	inner join student_group
+		on student_group.speciality = professor.speciality
+where professor.department = 'ЭВМ';
+
 -- Получить специальности студенческой группы, на которых работают преподаватели кафедры АСУ.
+select distinct student_group.speciality from student_group
+	inner join studying_proccess
+		on studying_proccess.group_id = student_group.group_number
+			inner join professor 
+				on professor.professors_number = studying_proccess.professors_id
+		where professor.department = 'АСУ';
+
 -- Получить номера предметов, изучаемых группой АС-8.
+select distinct subject_id from studying_proccess
+	inner join student_group
+		on student_group.group_number = studying_proccess.group_id
+where student_group.title = 'АС-8';
+
 -- Получить номера студенческих групп, которые изучают те же предметы, что и студенческая группа АС-8.
+
 -- Получить номера студенческих групп, которые не изучают предметы, преподаваемых в студенческой группе АС-8.
 -- Получить номера студенческих групп, которые не изучают предметы, преподаваемых преподавателем 430Л.
 -- Получить номера преподавателей, работающих с группой Э-15, но не преподающих предмет 12П.
